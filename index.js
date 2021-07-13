666ecb664c1e539fff9450b6e1f999c4b0073b64
Munsa1 <munsamibenge65@gmail.com>
2021-07-13 10:34:16 +0200

    Add local storage code

--- index.js
+++ index.js
@@ -12,5 +12,24 @@ const removeBook = (ev) => {
   const buttonId = ev.target.id;
   books = books.filter((y) => y !== books[books.findIndex((x) => x.id === parseInt(buttonId, 10))]);
   localStorage.setItem('bookObject', JSON.stringify(books));
+  document.getElementById('result-set').innerHTML = `${books.map(listBooks).join('')}`;
+};
+
+const addBooks = (ev) => {
+  ev.preventDefault();
+  k = k + 1;
+  const book = {
+    id: k,
+    title: document.getElementById('book-title').value,
+    author: document.getElementById('book-author').value,
+  };
+  books.push(book);
+  localStorage.setItem('bookObject', JSON.stringify(books));
   document.getElementById('bookList').innerHTML = `${books.map(listBooks).join('')}`;
+  document.getElementById('bookList').addEventListener('click', (e) => {
+    if (e.target.classList.contains('remove-button')) {
+      removeBook(e);
+    }
+  });
+  document.getElementById('addBooks').reset();
 };
