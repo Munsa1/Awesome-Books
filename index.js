54730b1a9867a0c1839fd2caebf4c84295d65635
Munsa1 <munsamibenge65@gmail.com>
2021-07-13 10:19:02 +0200

    Add remove book function

--- index.js
+++ index.js
@@ -1,76 +1,16 @@
-const books= document.getElementById('result-set');
-const form= document.getElementById('books-form');
-const title= document.getElementById('book-title');
-const author= document.getElementById('book-author');
-const addBtn= document.getElementById('Submit-btn');
+let myBooks = []
+let k = 0;
 
-let formData={
-  title:'',
-  author: '',
+function bookList (book){
+  return `
+  <li>${book.title}</li>
+  <li>${book.author}</li>
+  <button type='button' id='${book.id}' class='remove-button'>Remove</button>`;
 }
 
-class BookCollection {
-  constructor () {
-    this.collection = []
-  }
-
-  addBook(){
-    if (localStorage.getItem('bookdata')){
-      this.collection =  JSON.parse(localStorage.getItem('bookdata'));
-      this.collection.unshift(formData);
-      localStorage.setItem('bookdata', JSON.stringify(this.collection));
-      this.showBooks(this.BookCollection);
-    } else {
-      localStorage.setItem('bookdata', JSON.stringify([formData]))
-      showBooks( JSON.parse(localStorage.getItem('bookdata')));
-    }
-    author.value='';
-    title.value='';
-  }
-
-  showBooks(arr) {
-  const htmlCode = arr.map((book) => `
-    <li>
-      <span>"${book.book}" by &nbsp;</span>
-      <span class="author">${book.author}</span>
-      <button type="button" data-id="${book.id}">Remove</button>
-    </li>
-  `).join('');
-  books.innerHTML = htmlCode;
-
-    const removeButtons = books.querySelectorAll('button');
-    removeButtons.forEach((book) => {
-      book.addEventListener('click', (event) => {
-        this.removeBook(event.target.dataset.id);
-      })
-   });
-  }
-
-  removeBook(bookId) {
-    this.collection = JSON.parse(localStorage.getItem('bookdata')).filter((item) =>item.id!=bookId);
-    localStorage.setItem('bookdata', JSON.stringify(this.collection));
-    this.showBooks(this.collection);
-  }
-}
-
-const library = new BookCollection();
-
-window.addEventListener('load', () => {
-  if (localStorage.getItem('bookdata')){
-    const books =  JSON.parse(localStorage.getItem('bookdata'));
-    library.showBooks(books);
-  } 
-})
-
-
-const formInputs = form.querySelectorAll('input[type="text"]');
-formInputs.forEach((input) => {
-  input.addEventListener('input', (event) => {
-   formData={...formData, [event.target.id] : event.target.value, id:Date.now()}
-  })
-})
-
-form.addEventListener('button', (evt) => {
-  evt.preventDefault();
-  library.addBook();
-})
+const removeBook = (ev) => {
+  const buttonId = ev.target.id;
+  books = books.filter((y) => y !== books[books.findIndex((x) => x.id === parseInt(buttonId, 10))]);
+  localStorage.setItem('bookObject', JSON.stringify(books));
+  document.getElementById('bookList').innerHTML = `${books.map(listBooks).join('')}`;
+};
