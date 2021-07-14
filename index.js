72a09792bc5282b765e6c1ab1d57b0322f594c39
Munsa1 <munsamibenge65@gmail.com>
2021-07-14 23:11:52 +0200

    Final linter error check

--- index.js
+++ index.js
@@ -1,60 +1,63 @@
-
 class BookList {
   constructor() {
     this.list = [];
   }
+
   addBook(book) {
-    this.list.push(book)
+    this.list.push(book);
     localStorage.setItem('books', JSON.stringify(this.list));
     this.displayBooks();
   }
+
   removeBook(id) {
-    this.list = this.list.filter(book => book.id != id);
+    this.list = this.list.filter((book) => book.id !== id);
     localStorage.setItem('books', JSON.stringify(this.list));
-    this.displayBooks()
+    this.displayBooks();
   }
+
   displayBooks() {
     const result = document.getElementById('Result');
     result.innerHTML = '';
     this.list.forEach((book) => {
       result.appendChild(this.bookElement(book));
-    })
+    });
   }
-  bookElement(book){
+
+  bookElement(book) {
     const li = document.createElement('li');
     const title = document.createElement('h5');
     const author = document.createElement('h5');
     const removeBtn = document.createElement('button');
-    removeBtn.addEventListener('click',() => this.removeBook(book.id));
+    removeBtn.addEventListener('click', () => this.removeBook(book.id));
     title.innerText = book.title;
     author.innerText = book.author;
     removeBtn.innerText = 'remove';
-    title.classList.add("title")
+    title.classList.add('title');
     li.appendChild(title);
     li.appendChild(author);
     li.appendChild(removeBtn);
     return li;
   }
 }
-const library = new BookList()
+const library = new BookList();
 
-window.addEventListener("load", () => {
- const form = document.getElementById('booksForm')
- form.addEventListener('submit', (event) => {
-   event.preventDefault()
-   const title = document.getElementById('bookTitle');
-   const author = document.getElementById('bookAuthor');
-   const book = {
-     title: title.value,
-     author: author.value,
-     id: Date.now(),
+window.addEventListener('load', () => {
+  const form = document.getElementById('booksForm');
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const title = document.getElementById('bookTitle');
+    const author = document.getElementById('bookAuthor');
+    const book = {
+      title: title.value,
+      author: author.value,
+      id: Date.now(),
 
-   }
-   library.addBook(book);
-  })
+    };
+    library.addBook(book);
+  });
   const books = localStorage.getItem('books');
-  if(books){
-    library.list = JSON.parse(books); 
+  if (books) {
+    library.list = JSON.parse(books);
   }
   library.displayBooks();
- });
+});
