cdde5c49e6cc7905800c62b7e58da409974869d6
sheylaPozo <shiggipoke@gmail.com>
2021-07-16 05:56:36 -0500

    add html sckeleton of the page with all the respective elements contact info, nav bar,etc

--- index.js
+++ index.js
@@ -61,3 +61,9 @@ window.addEventListener('load', () => {
   }
   library.displayBooks();
 });
+
+n =  new Date();
+y = n.getFullYear();
+m = n.getMonth() + 1;
+d = n.getDate();
+document.getElementById("date").innerHTML = m + "/" + d + "/" + y;
