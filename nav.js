487047fbcb87d9944b3e6290b24b1ec6ef4a12ab
Munsa1 <munsamibenge65@gmail.com>
2021-07-16 16:44:33 +0200

    Add final styling

--- nav.js
+++ nav.js
@@ -1,20 +1,20 @@
 const tabs = document.querySelectorAll('nav-item');
 
 tabs.forEach((tab) => {
-     tab.addEventListener('click', (event) =>{
-     tab.preventDefault();
-     document.querySelector('select').classList.remove('select')
-     tab.classList.add('select');
-     const iD = tab.getAttribute('href').slice('1');
-     const selectedSection = document.querySelector('selected-tab');
+  tab.addEventListener('click', (event) => {
+    tab.preventDefault();
+    document.querySelector('select').classList.remove('select');
+    tab.classList.add('select');
+    const iD = tab.getAttribute('href').slice('1');
+    const selectedSection = document.querySelector('selected-tab');
 
-     if (selectedSection){
-         selectedSection.classList.remove('selected-tab');
-     }
+    if (selectedSection) {
+      selectedSection.classList.remove('selected-tab');
+    }
 
-     const section = document.getElementById(iD);
-     if (section){
-         section.classList.add('selected-tab');
-        }
-    });
+    const section = document.getElementById(iD);
+    if (section) {
+      section.classList.add('selected-tab');
+    }
+  });
 });
