619f3b44a4f2213bb4f6b90f3e5ef55b090c98cd
Munsa1 <munsamibenge65@gmail.com>
2021-07-16 16:19:57 +0200

    Add book collection class

--- index.js
+++ index.js
@@ -9,3 +9,53 @@ let formData={
     title:'',
     author: '',
   }
+
+  class BookCollection {
+    constructor () {
+      this.collection = []
+    }
+  
+    addBook(){
+      if (localStorage.getItem('bookdata')){
+        this.collection =  JSON.parse(localStorage.getItem('bookdata'));
+        
+        if (!this.collection.some(a=>a.author==formData.author && a.title==formData.title)) {
+          this.collection.unshift(formData);
+        }      
+        localStorage.setItem('bookdata', JSON.stringify(this.collection));
+        this.showBooks(this.collection);
+      } else {
+        localStorage.setItem('bookdata', JSON.stringify([formData]))
+        this.showBooks( JSON.parse(localStorage.getItem('bookdata')));
+      }
+      author.value='';
+      title.value='';
+    }
+  
+    showBooks(arr) {
+    const htmlCode = arr.map((book) => `
+      <li>
+        <span>"${book.title}" by &nbsp;</span>
+        <span class="author">${book.author}</span>
+        <button type="button" data-id="${book.id}">Remove</button>
+      </li>
+    `).join('');
+    books.innerHTML = htmlCode;
+  
+      const removeButtons = books.querySelectorAll('button');
+      removeButtons.forEach((book) => {
+        book.addEventListener('click', (event) => {
+          this.removeBook(event.target.dataset.id);
+        })
+     });
+    }
+  
+    removeBook(bookId) {
+      this.collection = JSON.parse(localStorage.getItem('bookdata')).filter((item) =>item.id!=bookId);
+      localStorage.setItem('bookdata', JSON.stringify(this.collection));
+      this.showBooks(this.collection);
+      if (this.collection.length===0){
+        books.innerHTML='<li>No books</li>'
+      }
+    }
+  }
