44447d2a0518139038c0fa8b23f8ca50b63ee04a
Munsa1 <munsamibenge65@gmail.com>
2021-07-16 13:27:41 +0200

    Add nav.js file with navigation code

---
+++ nav.js
@@ -0,0 +1,10 @@
+const tabs = document.querySelectorAll('nav-item');
+
+tabs.forEach((tab) => {
+     tab.addEventListener('click', (event) =>{
+     tab.preventDefault();
+     document.querySelector('select').classList.remove('select')
+     tab.classList.add('select');
+     const iD = tab.getAttribute('href').slice('1')
+     });
+});
