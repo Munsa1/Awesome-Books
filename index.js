4d65844441d7d012fa5319c10385ccf1827e897c
Munsa1 <munsamibenge65@gmail.com>
2021-07-16 16:15:12 +0200

    Add formData object

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ const title= document.getElementById('title');
 const author= document.getElementById('author');
 const addBtn= document.getElementById('addBtn');
 const date = document.getElementId('date');
+
+let formData={
+    title:'',
+    author: '',
+  }
